39b11678ea22831334d10b6bcb1c3ed8e89e0bcb
Sam Goodger <sam@goodger.nz>
2019-12-10 23:02:38 +1100

    Add ability to query another player's join date

--- src/main/java/com/turbotailz/joindate/Commands.java
+++ src/main/java/com/turbotailz/joindate/Commands.java
@@ -1,12 +1,15 @@
 package com.turbotailz.joindate;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import static org.bukkit.Bukkit.getLogger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Commands implements CommandExecutor {
 
@@ -14,17 +17,77 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
         if (cmd.getName().equalsIgnoreCase("joindate")) {
+            Player player = null;
+
             if (args.length == 0) {
                 if (sender instanceof Player) {
-                    Player player = (Player) sender;
+                    player = (Player) sender;
+                } else {
+                    sender.sendMessage("Only in-game players can check their join date!");
 
-                    String joinDate = new java.text.SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z").format(new java.util.Date(player.getFirstPlayed()));
+                    return true;
+                }
+            } else if (args.length == 1) {
+                String playerName = args[0];
 
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', ("&7&oYou joined the server on &a&o" + joinDate)));
+                player = Bukkit.getPlayer(playerName);
+
+                if (player == null) {
+                    Bukkit.getScheduler().runTaskAsynchronously(JoinDate.getPlugin(), () -> {
+                        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerName);
+
+                        sendJoinDate(sender, offlinePlayer);
+                    });
                 }
             }
+
+            if (player == null) return true;
+
+            return sendJoinDate(sender, player);
         }
 
+        return true;
+    }
+
+    private boolean sendJoinDate(CommandSender sender, Object player) {
+        Date date = null;
+
+        if (player instanceof Player) {
+            date = new Date(((Player) player).getFirstPlayed());
+        } else if (player instanceof OfflinePlayer) {
+            long offlineDate = ((OfflinePlayer) player).getFirstPlayed();
+
+            if (offlineDate == 0) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ("&7That player has not joined the server before.")));
+
+                return true;
+            }
+
+            date = new Date(offlineDate);
+        }
+
+
+
+        String joinDate = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z")
+                .format(date);
+
+        String string;
+
+        if (sender == player) {
+            string = ChatColor.translateAlternateColorCodes('&', ("&7You joined the server on &a" + joinDate));
+        } else {
+            String playerName = null;
+
+            if (player instanceof Player) {
+                playerName = ((Player) player).getName();
+            } else if (player instanceof OfflinePlayer) {
+                playerName = ((OfflinePlayer) player).getName();
+            }
+
+            string = ChatColor.translateAlternateColorCodes('&', ("&7" + playerName + " joined the server on &a" + joinDate));
+        }
+
+        sender.sendMessage(string);
         return true;
     }
 }
--- src/main/java/com/turbotailz/joindate/JoinDate.java
+++ src/main/java/com/turbotailz/joindate/JoinDate.java
@@ -4,9 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JoinDate extends JavaPlugin {
 
+    private static JoinDate instance;
+
+    public static JoinDate getPlugin() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
         this.getCommand("joindate").setExecutor(new Commands());
     }
 
