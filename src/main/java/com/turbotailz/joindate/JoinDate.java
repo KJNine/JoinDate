e741908594d7e14d3a4327a1cbbd53390d442507
KJNine <kjnine@kjnine.net>
2022-09-25 21:34:18 -0400

    Change file-saving to json format.

--- src/main/java/com/turbotailz/joindate/JoinDate.java
+++ src/main/java/com/turbotailz/joindate/JoinDate.java
@@ -1,5 +1,6 @@
 package com.turbotailz.joindate;
 
+import com.google.gson.*;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -10,10 +11,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.io.*;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -26,7 +24,7 @@ public final class JoinDate extends JavaPlugin implements Listener {
     }
 
     public HashMap<UUID, Integer> jnMap;
-    private File df = new File(getDataFolder(), "data.bin"); // apparently java doesn't store it in human-readable format
+    private File df = new File(getDataFolder(), "data.json");
     private BukkitTask autosaveTask;
 
     @Override
@@ -43,8 +41,12 @@ public final class JoinDate extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 
         if(df.exists()) {
-            try (FileInputStream fis = new FileInputStream(df); ObjectInputStream ois = new ObjectInputStream(fis)) {
-                jnMap = (HashMap<UUID, Integer>) ois.readObject();
+            try (FileReader fr = new FileReader(df)) {
+                JsonObject json = new JsonParser().parse(fr).getAsJsonObject();
+                jnMap = new HashMap<>(json.entrySet().stream()
+                        .collect(Collectors.<Map.Entry<String, JsonElement>, UUID, Integer>toMap(
+                                e -> UUID.fromString(e.getKey()),
+                                e -> e.getValue().getAsInt())));
             } catch (Exception e) {
                 getLogger().log(Level.WARNING, "Exception loading Join-Numbers data file (proceeding with re-calculated data). ", e);
             }
@@ -64,9 +66,11 @@ public final class JoinDate extends JavaPlugin implements Listener {
 
     public void saveData() {
         synchronized (jnMap) {
-            try (FileOutputStream fos = new FileOutputStream(df); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-                oos.writeObject(jnMap);
-                oos.flush();
+            try (FileWriter fw = new FileWriter(df)) {
+                Gson gson = new Gson();
+                JsonObject jo = new JsonObject();
+                jnMap.forEach((u, i) -> jo.add(u.toString(), new JsonPrimitive(i)));
+                gson.toJson(jo, fw);
             } catch(IOException e) {
                 getLogger().log(Level.WARNING, "Exception saving Join-Numbers data file. ", e);
             }
